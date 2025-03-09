2475eab673a9e7697c26c70079ce5a076ade4b89
Ati411 <athiwat.t@kkumail.com>
2025-03-09 19:37:45 +0700

    New Code

--- PokemonDexx/Form1.Designer.cs
+++ PokemonDexx/Form1.Designer.cs
@@ -72,104 +72,116 @@
             // label1
             // 
             label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Variable Display", 9F, FontStyle.Bold);
             label1.Location = new Point(151, 45);
             label1.Name = "label1";
-            label1.Size = new Size(23, 15);
+            label1.Size = new Size(23, 16);
             label1.TabIndex = 1;
             label1.Text = "HP";
             // 
             // label2
             // 
             label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Variable Display", 9F, FontStyle.Bold);
             label2.Location = new Point(151, 86);
             label2.Name = "label2";
-            label2.Size = new Size(27, 15);
+            label2.Size = new Size(29, 16);
             label2.TabIndex = 2;
             label2.Text = "ATK";
             // 
             // label3
             // 
             label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI Variable Display", 9F, FontStyle.Bold);
             label3.Location = new Point(151, 132);
             label3.Name = "label3";
-            label3.Size = new Size(27, 15);
+            label3.Size = new Size(28, 16);
             label3.TabIndex = 3;
             label3.Text = "DEF";
             // 
             // label4
             // 
             label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI Variable Display", 9F, FontStyle.Bold);
             label4.Location = new Point(142, 182);
             label4.Name = "label4";
-            label4.Size = new Size(46, 15);
+            label4.Size = new Size(49, 16);
             label4.TabIndex = 4;
             label4.Text = "Spl.ATK";
             // 
             // label5
             // 
             label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI Variable Display", 9F, FontStyle.Bold);
             label5.Location = new Point(142, 236);
             label5.Name = "label5";
-            label5.Size = new Size(46, 15);
+            label5.Size = new Size(48, 16);
             label5.TabIndex = 5;
             label5.Text = "Spl.DEF";
             // 
             // label6
             // 
             label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI Variable Display", 9F, FontStyle.Bold);
             label6.Location = new Point(674, 45);
             label6.Name = "label6";
-            label6.Size = new Size(39, 15);
+            label6.Size = new Size(42, 16);
             label6.TabIndex = 6;
             label6.Text = "Speed";
             // 
             // label7
             // 
             label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI Variable Display", 9F, FontStyle.Bold);
             label7.Location = new Point(674, 86);
             label7.Name = "label7";
-            label7.Size = new Size(43, 15);
+            label7.Size = new Size(45, 16);
             label7.TabIndex = 7;
             label7.Text = "Height";
             // 
             // label8
             // 
             label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI Variable Display", 9F, FontStyle.Bold);
             label8.Location = new Point(674, 132);
             label8.Name = "label8";
-            label8.Size = new Size(45, 15);
+            label8.Size = new Size(48, 16);
             label8.TabIndex = 8;
             label8.Text = "Weight";
             // 
             // label9
             // 
             label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI Variable Display", 9F, FontStyle.Bold);
             label9.Location = new Point(681, 182);
             label9.Name = "label9";
-            label9.Size = new Size(32, 15);
+            label9.Size = new Size(34, 16);
             label9.TabIndex = 9;
             label9.Text = "Total";
             // 
             // label10
             // 
             label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI Variable Display", 9F, FontStyle.Bold);
             label10.Location = new Point(416, 324);
             label10.Name = "label10";
-            label10.Size = new Size(39, 15);
+            label10.Size = new Size(40, 16);
             label10.TabIndex = 10;
             label10.Text = "Name";
             // 
             // label11
             // 
             label11.AutoSize = true;
+            label11.Font = new Font("Segoe UI Variable Display", 9F, FontStyle.Bold);
             label11.Location = new Point(416, 361);
             label11.Name = "label11";
-            label11.Size = new Size(31, 15);
+            label11.Size = new Size(34, 16);
             label11.TabIndex = 11;
             label11.Text = "Type";
             // 
             // btnPikachu
             // 
+            btnPikachu.Font = new Font("Segoe UI Emoji", 9F, FontStyle.Bold);
             btnPikachu.Location = new Point(94, 403);
             btnPikachu.Name = "btnPikachu";
             btnPikachu.Size = new Size(220, 55);
@@ -180,6 +192,7 @@
             // 
             // btnSnorlax
             // 
+            btnSnorlax.Font = new Font("Segoe UI Emoji", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
             btnSnorlax.Location = new Point(372, 403);
             btnSnorlax.Name = "btnSnorlax";
             btnSnorlax.Size = new Size(220, 55);
@@ -190,6 +203,7 @@
             // 
             // btnDiglett
             // 
+            btnDiglett.Font = new Font("Segoe UI Emoji", 9F, FontStyle.Bold);
             btnDiglett.Location = new Point(660, 403);
             btnDiglett.Name = "btnDiglett";
             btnDiglett.Size = new Size(220, 55);
@@ -200,6 +214,7 @@
             // 
             // btnSwirlix
             // 
+            btnSwirlix.Font = new Font("Segoe UI Emoji", 9F, FontStyle.Bold);
             btnSwirlix.Location = new Point(94, 502);
             btnSwirlix.Name = "btnSwirlix";
             btnSwirlix.Size = new Size(220, 55);
@@ -210,6 +225,7 @@
             // 
             // btnLuvdisc
             // 
+            btnLuvdisc.Font = new Font("Segoe UI Emoji", 9F, FontStyle.Bold);
             btnLuvdisc.Location = new Point(372, 502);
             btnLuvdisc.Name = "btnLuvdisc";
             btnLuvdisc.Size = new Size(220, 55);
@@ -220,6 +236,7 @@
             // 
             // btnUnown
             // 
+            btnUnown.Font = new Font("Segoe UI Emoji", 9F, FontStyle.Bold);
             btnUnown.Location = new Point(660, 502);
             btnUnown.Name = "btnUnown";
             btnUnown.Size = new Size(220, 55);
@@ -231,99 +248,110 @@
             // lbHP
             // 
             lbHP.AutoSize = true;
+            lbHP.Font = new Font("Segoe UI Historic", 9F, FontStyle.Bold | FontStyle.Italic);
             lbHP.Location = new Point(219, 45);
             lbHP.Name = "lbHP";
-            lbHP.Size = new Size(44, 15);
+            lbHP.Size = new Size(51, 15);
             lbHP.TabIndex = 18;
             lbHP.Text = "label12";
             // 
             // lbATK
             // 
             lbATK.AutoSize = true;
+            lbATK.Font = new Font("Segoe UI Historic", 9F, FontStyle.Bold | FontStyle.Italic);
             lbATK.Location = new Point(219, 86);
             lbATK.Name = "lbATK";
-            lbATK.Size = new Size(44, 15);
+            lbATK.Size = new Size(51, 15);
             lbATK.TabIndex = 19;
             lbATK.Text = "label13";
             // 
             // lbDEF
             // 
             lbDEF.AutoSize = true;
+            lbDEF.Font = new Font("Segoe UI Historic", 9F, FontStyle.Bold | FontStyle.Italic);
             lbDEF.Location = new Point(219, 132);
             lbDEF.Name = "lbDEF";
-            lbDEF.Size = new Size(44, 15);
+            lbDEF.Size = new Size(51, 15);
             lbDEF.TabIndex = 20;
             lbDEF.Text = "label14";
             // 
             // lbSpl_ATK
             // 
             lbSpl_ATK.AutoSize = true;
+            lbSpl_ATK.Font = new Font("Segoe UI Historic", 9F, FontStyle.Bold | FontStyle.Italic);
             lbSpl_ATK.Location = new Point(219, 182);
             lbSpl_ATK.Name = "lbSpl_ATK";
-            lbSpl_ATK.Size = new Size(44, 15);
+            lbSpl_ATK.Size = new Size(51, 15);
             lbSpl_ATK.TabIndex = 21;
             lbSpl_ATK.Text = "label15";
             // 
             // lbSpl_DEF
             // 
             lbSpl_DEF.AutoSize = true;
+            lbSpl_DEF.Font = new Font("Segoe UI Historic", 9F, FontStyle.Bold | FontStyle.Italic);
             lbSpl_DEF.Location = new Point(219, 236);
             lbSpl_DEF.Name = "lbSpl_DEF";
-            lbSpl_DEF.Size = new Size(44, 15);
+            lbSpl_DEF.Size = new Size(51, 15);
             lbSpl_DEF.TabIndex = 22;
             lbSpl_DEF.Text = "label16";
             // 
             // lbName
             // 
             lbName.AutoSize = true;
+            lbName.Font = new Font("Segoe UI Historic", 9F, FontStyle.Bold | FontStyle.Italic);
             lbName.Location = new Point(486, 324);
             lbName.Name = "lbName";
-            lbName.Size = new Size(44, 15);
+            lbName.Size = new Size(51, 15);
             lbName.TabIndex = 23;
             lbName.Text = "label17";
             // 
             // lbType
             // 
             lbType.AutoSize = true;
+            lbType.Font = new Font("Segoe UI Historic", 9F, FontStyle.Bold | FontStyle.Italic);
             lbType.Location = new Point(486, 361);
             lbType.Name = "lbType";
-            lbType.Size = new Size(44, 15);
+            lbType.Size = new Size(51, 15);
             lbType.TabIndex = 24;
             lbType.Text = "label18";
             // 
             // lbSpeed
             // 
             lbSpeed.AutoSize = true;
+            lbSpeed.Font = new Font("Segoe UI Historic", 9F, FontStyle.Bold | FontStyle.Italic);
             lbSpeed.Location = new Point(765, 45);
             lbSpeed.Name = "lbSpeed";
-            lbSpeed.Size = new Size(44, 15);
+            lbSpeed.Size = new Size(51, 15);
             lbSpeed.TabIndex = 25;
             lbSpeed.Text = "label19";
             // 
             // lbHeight
             // 
             lbHeight.AutoSize = true;
+            lbHeight.Font = new Font("Segoe UI Historic", 9F, FontStyle.Bold | FontStyle.Italic);
             lbHeight.Location = new Point(765, 86);
             lbHeight.Name = "lbHeight";
-            lbHeight.Size = new Size(44, 15);
+            lbHeight.Size = new Size(51, 15);
             lbHeight.TabIndex = 26;
             lbHeight.Text = "label20";
             // 
             // lbWeight
             // 
             lbWeight.AutoSize = true;
+            lbWeight.Font = new Font("Segoe UI Historic", 9F, FontStyle.Bold | FontStyle.Italic);
             lbWeight.Location = new Point(765, 132);
             lbWeight.Name = "lbWeight";
-            lbWeight.Size = new Size(44, 15);
+            lbWeight.Size = new Size(51, 15);
             lbWeight.TabIndex = 27;
             lbWeight.Text = "label21";
             // 
             // lbTotal
             // 
             lbTotal.AutoSize = true;
+            lbTotal.Font = new Font("Segoe UI Historic", 9F, FontStyle.Bold | FontStyle.Italic);
             lbTotal.Location = new Point(765, 182);
             lbTotal.Name = "lbTotal";
-            lbTotal.Size = new Size(44, 15);
+            lbTotal.Size = new Size(51, 15);
             lbTotal.TabIndex = 28;
             lbTotal.Text = "label22";
             // 
